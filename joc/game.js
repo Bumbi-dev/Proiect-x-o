7afc5bca09f1003529029a5a2f8825d96043ad77
Manu <progamerro12345@gmail.com>
2025-10-20 15:27:10 +0300

    Ui improvement

--- joc/game.js
+++ joc/game.js
@@ -1,6 +1,18 @@
 let casute = [0, 0, 0, 0, 0, 0, 0, 0, 0];
 
-for (let i = 1; i <= 9; i++) {
-    document.getElementById(i.toString()).textContent = casute[i-1].toString()
-}
+let player_turn = 1
 
+const cells = document.querySelectorAll('.cell');
+cells.forEach(cell => {
+  cell.addEventListener('click', () => {
+    if(cell.textContent != '')
+        return;
+
+    if(player_turn == 1)
+        cell.textContent = 'X';
+    else
+        cell.textContent = '0'
+
+    player_turn = -player_turn;
+  });
+});
