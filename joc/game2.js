f1c5d429dbb6eef4e03960b46391035867137f01
AlexFirica <alex.firica1977@gmail.com>
2025-10-21 16:17:06 +0300

    putine modificari

---
+++ joc/game2.js
@@ -0,0 +1,15 @@
+/*se apasa un buton si il blocheaza pe celalalt.
+var disableX = document.getElementById("selectX");  
+var disableO = document.getElementById("selectO"); 
+const selectPlayer = (player) => {
+	if (player == AI_turn) {
+		player_turn = 'O';
+		AI_turn = 'X';
+    disableX.disabled=true;
+	} else {
+		player_turn = 'X';
+		AI_turn = 'O';
+    disableO.disabled=true;
+	}
+}; */
+
