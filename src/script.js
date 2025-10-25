5e90f175a5498bb5d43095c7fc4ebcb3025b84fe
viic999 <vectorvoicu2006@gmail.com>
2025-10-25 15:27:19 +0300

    Animatie + Bordura + Fundal oribil

--- src/script.js
+++ src/script.js
@@ -17,3 +17,49 @@ function submit() {
 
     location.href="../joc/game.html"
 }
+
+const bouncers = [
+  {el: document.getElementById('bouncerX'), x: 100, y: 100, dx: 3, dy: 3},
+  {el: document.getElementById('bouncerO'), x: 300, y: 200, dx: 2, dy: 2}
+];
+
+function animateBouncers() {
+  const width = window.innerWidth;
+  const height = window.innerHeight;
+
+  bouncers.forEach(obj => {
+    const rect = obj.el.getBoundingClientRect(); 
+
+    obj.x += obj.dx;
+    obj.y += obj.dy;
+
+    const padding = 2;
+
+    
+    if (obj.x + rect.width >= width - padding) {
+      obj.x = width - rect.width - padding; 
+      obj.dx *= -1;
+    }
+    if (obj.x <= padding) {
+      obj.x = padding;
+      obj.dx *= -1;
+    }
+
+    
+    if (obj.y + rect.height >= height - padding) {
+      obj.y = height - rect.height - padding; 
+      obj.dy *= -1;
+    }
+    if (obj.y <= padding) {
+      obj.y = padding;
+      obj.dy *= -1;
+    }
+
+    obj.el.style.left = obj.x + 'px';
+    obj.el.style.top = obj.y + 'px';
+  });
+
+  requestAnimationFrame(animateBouncers);
+}
+
+animateBouncers();
